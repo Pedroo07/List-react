8595011b23dbb6b539f7602c5256d2aa983b84e1
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-20 11:01:35 -0300

    Exibir itens da lista na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Plus, Search, FileDown } from "lucide-react"
+import { Plus, Search, FileDown, MoreHorizontal } from "lucide-react"
 import { Header } from "./components/header"
 import { Tabs } from "./components/tabs"
 import { Button } from "./components/ui/button"
 import { Control, Input } from "./components/ui/input"
+import { Table, TableBody, TableCell } from "./components/ui/table"
+import { TableHead, TableHeader, TableRow } from "./components/ui/table"
 
 export const App = () => {
   return (
@@ -29,6 +31,39 @@ export const App = () => {
             Export
           </Button>
         </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+           {Array.from({length: 10 }).map((value, index) => {
+            return(
+              <TableRow key={index}>
+              <TableCell> </TableCell>
+              <TableCell>
+                <div>
+                  <div className="flex flex-col gap-0.5">
+                    <span className="font-medium">React</span>
+                    <span className="font-xs text-zinc-500">a8a2af47-1ff7-4cb0-846e-1c7d38e538b9</span>
+                  </div>
+                </div>
+              </TableCell>
+              <TableCell className="text-zinc-300"> 13 video(s)</TableCell>
+              <TableCell className="text-right">
+                <Button size="icon">
+                  <MoreHorizontal className="size-4" />
+                </Button>
+              </TableCell>
+            </TableRow>
+            )
+           })}
+          </TableBody>
+        </Table>
       </main>
     </div>
   )
