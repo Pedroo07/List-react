8bfeb95a537503f51aae65acc063fa0f926bd67b
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-21 17:14:10 -0300

    Criar função para validação do slug

--- src/components/createTagForm.tsx
+++ src/components/createTagForm.tsx
@@ -4,20 +4,36 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
+import slugify from 'slugify'
 
 const createTagSchema = z.object({
     name: z.string().min(3, { message: 'Minimium 3 caractres' }),
     slug: z.string()
 })
+
 type CreateTagSchema = z.infer<typeof createTagSchema>
-export const CreateTagForm = () => {
-    const { register, handleSubmit } = useForm<CreateTagSchema>({
+
+function getSlugFromString(input: string): string {
+    if (!input) {
+        return 'nao conseguio ler'; 
+    }
+    return input
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .replace(/[^\w\s]/g, '')
+        .replace(/\s+/g, '-');
+}
+ export const CreateTagForm = () => {
+    const { register, handleSubmit, watch } = useForm<CreateTagSchema>({
         resolver: zodResolver(createTagSchema)
     })
-
     const createTag = (data: CreateTagSchema) => {
         console.log(data)
     }
+
+    const slug = getSlugFromString(watch('name'))
+
     return (
         <form className='w-full space-y-6' onSubmit={handleSubmit(createTag)} >
             <div className='space-y-2'>
@@ -26,7 +42,7 @@ export const CreateTagForm = () => {
             </div>
             <div className='space-y-2'>
                 <label className='text-sm font-medium block' htmlFor='slug'>Slug</label>
-                <input {...register('slug')} type="text" readOnly id='slug' className='border-zinc-800 rounded-lg px-3 py-2 block bg-zinc-800/50 w-full' />
+                <input {...register('slug')} type="text" readOnly id='slug' value={slug} className='border-zinc-800 rounded-lg px-3 py-2 block bg-zinc-800/50 w-full' />
             </div>
             <div className='flex items-center justify-end gap-2'>
                 <Dialog.Close asChild>
