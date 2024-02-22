a9171f374749c55fda3f31748c5dbdbb910e93d9
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-21 22:24:05 -0300

    Exibir novo item na tela quando o usuario criar automaticamente

--- src/components/createTagForm.tsx
+++ src/components/createTagForm.tsx
@@ -1,10 +1,11 @@
 import { Button } from './ui/button'
 import { Check, Loader2, X } from 'lucide-react'
-import {  z } from 'zod'
+import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
 import slugify from 'slugify'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 const createTagSchema = z.object({
     title: z.string().min(3, { message: 'Minimium 3 caracteres' })
@@ -18,26 +19,34 @@ function getSlugFromString(input: string): string {
         remove: /[*+~.()'"!:@]/g
     });
 }
- export const CreateTagForm = () => {
+export const CreateTagForm = () => {
     const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
         resolver: zodResolver(createTagSchema)
     })
 
     const slug = watch('title') ? getSlugFromString(watch('title')) : ''
+    const queryClient = useQueryClient()
 
-    const createTag = async ({title}: CreateTagSchema) => {
-        console.log({title , slug})
+    const {mutateAsync } = useMutation({
+        mutationFn: async({ title }: CreateTagSchema) => {
+            await new Promise(resolve => setTimeout(resolve, 2000))
 
-        await new Promise(resolve =>  setTimeout(resolve, 2000))
-
-        await fetch('http://localhost:3333/tags', {
-            method: 'POST',
-            body: JSON.stringify({
-                title,
-                slug,
-                amountOfvideos: 0
+            await fetch('http://localhost:3333/tags', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title,
+                    slug,
+                    amountOfvideos: 0
+                })
             })
+        }, onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['get-tags']
+            })
+        }
         })
+    const createTag = async ({ title }: CreateTagSchema) => {
+        await mutateAsync({title})
     }
     return (
         <form className='w-full space-y-6' onSubmit={handleSubmit(createTag)} >
@@ -51,7 +60,7 @@ function getSlugFromString(input: string): string {
             <div className='space-y-2'>
                 <label className='text-sm font-medium block' htmlFor='slug'>Slug</label>
                 <input type="text" readOnly id='slug' value={slug} className='border-zinc-800 rounded-lg px-3 py-2 block bg-zinc-800/50 w-full' />
-                
+
             </div>
             <div className='flex items-center justify-end gap-2'>
                 <Dialog.Close asChild>
@@ -61,8 +70,8 @@ function getSlugFromString(input: string): string {
                     </Button>
                 </Dialog.Close>
                 <Button type='submit' disabled={formState.isSubmitting} className='bg-teal-400 text-teal-950'>
-                    {formState.isSubmitting ? <Loader2 className='size-3 animate-spin'/>  : <Check className='size-3' />}
-                    
+                    {formState.isSubmitting ? <Loader2 className='size-3 animate-spin' /> : <Check className='size-3' />}
+
                     Save
                 </Button>
             </div>
